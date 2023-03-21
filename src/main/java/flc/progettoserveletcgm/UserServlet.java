0323fc774c7b3bb965db78ea660c09e8a2765ec4
LordKazor <LordKazor@KazorCaverna2.mshome.net>
2023-03-21 10:32:31 +0100

    Rev 1.2

--- src/main/java/flc/progettoserveletcgm/UserServlet.java
+++ src/main/java/flc/progettoserveletcgm/UserServlet.java
@@ -8,12 +8,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//non ci sarà un main, poiché le classi che estendono servlet non ne hanno bisogno
+
 @WebServlet(name = "UserServlet", urlPatterns = {"/user"})
 public class UserServlet extends HttpServlet{
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-    
+        
+        //String getContextPath()
+        //resp.getOutputStream()
+        
+        //tramite get parameter posso richiedere i valori associati a determinate chiavi
+        System.out.println(req.getParameter("firstname"));
+        System.out.println(req.getParameter("age"));
+        
+        
         PrintWriter writer = resp.getWriter();
         writer.print("Pagina dell'utente e altre blablabla");
         
